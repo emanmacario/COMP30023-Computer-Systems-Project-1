482bf430457255c90873caf33de19b621bae850d
emanmacario <emacario1997@hotmail.com>
2018-04-19 08:07:04 +1000

    Created server.h and updated server.c to reflect this

--- server.c
+++ server.c
@@ -3,52 +3,30 @@
 #include <sys/socket.h>   // needed for socket function
 #include <netinet/in.h>   // needed for internet address i.e. sockaddr_in and in_addr
 #include <netdb.h>        // needed for setting up server i.e. AI_PASSIVE and shit
-
 #include <assert.h>
+#include <arpa/inet.h>  // needed for byte order functions
+#include <unistd.h>     // needed for read/close functions
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <arpa/inet.h>  // needed for byte order functions
-#include <unistd.h>     // needed for read/close functions
-
-
 #include <pthread.h>    // for POSIX threads
-
-#define BUFFER_SIZE 8192  // buffer size
-#define BACKLOG 10     // total pending connections queue will hold
+#include "server.h"
 
 
 
-// Function prototypes
-char *get_content_type(char *filename);
-void usage(char *prog_name);
-char *get_request_line(FILE *fdstream);
-char *get_filename(char *request_line);
-char *get_path_to_file(char *filename);
-char *get_content_type(char *filename);
-void *handle_http_request(void *arg);
-void send_response_body(int newfd, int fd);
-void send_response_head(int newfd, char *status_line, char *content_type);
-char *make_content_type_header(char *content_type);
-
-
-
-// Global variable, to be shared by all threads
-char *path_to_web_root = NULL;
-
-
-
-/* Gives user information on how
- * to use the program with corresponding
- * command line arguments.
+/** 
+ * Gives the user information on how to use the program 
+ * and the required input command line arguments.
  */
 void usage(char *prog_name) {
     printf("Usage: %s [port number] [path to web root]\n", prog_name);
 }
 
 
-// Reads in a request from the file descriptor associated with
-// a connection
+/**
+ * Extracts and returns the HTTP request line from a
+ * HTTP request sent by a client over a connection.
+ */
 char *get_request_line(FILE *fdstream) {
 
     if (fdstream == NULL) {
@@ -77,7 +55,9 @@ char *get_request_line(FILE *fdstream) {
  */
 char *get_filename(char *request_line) {
 
-    // Allocate and initialise memory for filename
+    // Allocate and initialise memory for filename.
+    // We know that the filename will fit into the
+    // size occupied by the entire request line.
     char *filename = malloc(sizeof(char)*strlen(request_line));
     if (filename == NULL) {
         perror("Error allocating memory for filename");
@@ -100,9 +80,79 @@ char *get_filename(char *request_line) {
 }
 
 
-// Gets the path to the file requested. Can either be an absolute
-// path or relative path, depending on the web root specified in
-// command line.
+
+/**
+ * Parses the name of the file the client requested
+ * and returns the MIME type (content type) associated
+ * with the file.
+ */
+char *get_content_type(char *filename) {
+
+    // The max length content type i.e. application/javascript
+    const size_t max_content_type_len = 22;
+
+    // Allocate and initialise memory for the content type.
+    char *content_type = malloc(sizeof(char)*(max_content_type_len+1)); // +1 for nullbyte
+    if (content_type == NULL) {
+        perror("Failed to allocate memory to content type");
+        exit(EXIT_FAILURE);
+    }
+    memset(content_type, '\0', max_content_type_len+1);
+
+    // Get the content type associated with the file extension.
+    if (strstr(filename, ".html") != NULL) {
+        strcpy(content_type, "text/html");
+    } else if (strstr(filename, ".jpg") != NULL) {
+        strcpy(content_type, "image/jpeg");
+    } else if (strstr(filename, ".css") != NULL) {
+        strcpy(content_type, "text/css");
+    } else if (strstr(filename, ".js") != NULL) {
+        strcpy(content_type, "application/javascript");
+    } else {
+        strcpy(content_type, "text/plain");
+    }
+
+    return content_type;
+}
+
+
+/**
+ * Given a content type,
+ */
+char *make_content_type_header(char *content_type) {
+    // Sanity checking
+    assert(content_type);
+
+    // The base size of the 'Content-Type' header,
+    // including two newline and one whitespace characters
+    const size_t base_size = 15;
+
+    // Calculate size of content type header (without nullbyte)
+    size_t content_type_header_size = base_size + strlen(content_type);
+
+    // Allocate and initialise memory for content type header
+    char *content_type_header;
+    content_type_header = malloc(sizeof(char)*(content_type_header_size+1));
+    if (content_type_header == NULL) {
+        perror("Error allocating memory for content type header");
+        exit(EXIT_FAILURE);
+    }
+    content_type_header[0] = '\0';
+
+    // Construct the header
+    strcat(content_type_header, "Content-Type: ");
+    strcat(content_type_header, content_type);
+    strcat(content_type_header, "\n");
+
+    return content_type_header;
+}
+
+
+/**
+ * Gets the path to the file requested. Can either be an absolute
+ * path or relative path, depending on the web root specified on
+ * the command line.
+ */
 char *get_path_to_file(char *filename) {
 
     // Calculate total length of path to the requested file
@@ -123,39 +173,116 @@ char *get_path_to_file(char *filename) {
 }
 
 
-
-
-/* Parses the filename the client wants, and
- * returns the MIME type (content type) associated
- * with the file's extension.
+/**
+ * Constructs the status line and headers to be sent in the HTTP
+ * response, and then actually sends them to the client.
  */
-char *get_content_type(char *filename) {
+void send_response_head(int newfd, char *status_line, char *content_type) {    
 
-    // The max length content type i.e. application/javascript
-    const size_t max_content_type_len = 22;
+    // First construct the 'Content-Type' header, using the given content type
+    char *content_type_header = make_content_type_header(content_type);
 
-    char *content_type = malloc(sizeof(char)*(max_content_type_len+1)); // +1 for nullbyte
-    if (content_type == NULL) {
-        perror("Failed to allocate memory to content type");
+    // Calculate the size of the response head, + 1 for newline.
+    size_t size = strlen(status_line) + strlen(content_type_header) + 1;
+
+    // Allocate memory for response, +1 for nullbyte
+    char *response_head = malloc(sizeof(char)*(size+1));
+    if (response_head == NULL) {
+        perror("Error allocating memory to HTTP response");
+        exit(EXIT_FAILURE);
+    }
+    response_head[0] = '\0';
+
+
+    // Construct the HTTP response message status, header, and empty lines
+    strcat(response_head, status_line);
+    strcat(response_head, content_type_header);
+    strcat(response_head, "\n");
+    
+
+    // Now send the actual HTTP response status line and headers
+    ssize_t total_bytes_sent = 0, bytes_sent;
+    while ((bytes_sent = send(newfd, response_head+total_bytes_sent, 
+                              size-total_bytes_sent, 0)) > 0) {
+        total_bytes_sent += bytes_sent;
+    }
+
+    // Check to see if we've successfully sent the response head
+    if (total_bytes_sent != size) {
+        fprintf(stderr, "Error sending response head\n");
         exit(EXIT_FAILURE);
     }
 
-    // Get the content type associated with the file extension.
-    if (strstr(filename, ".html") != NULL) {
-        strcpy(content_type, "text/html");
-    } else if (strstr(filename, ".jpg") != NULL) {
-        strcpy(content_type, "image/jpeg");
-    } else if (strstr(filename, ".css") != NULL) {
-        strcpy(content_type, "text/css");
-    } else if (strstr(filename, ".js") != NULL) {
-        strcpy(content_type, "application/javascript");
-    }
-    return content_type;
+    // Free the memory allocated to content type header and response head
+    free(content_type_header);
+    free(response_head);
 }
+
+
+
+/**
+ * Takes as input the file descriptors for an open connection
+ * with a client, and one for a file, and sends the contents
+ * of the file to the client.
+ */
+void send_response_body(int newfd, int fd) {
+
+    // Don't send anything if file does not exist.
+    if (fd < 0) {
+        return;
+    }
+
+    // Get the size of the file in bytes.
+    size_t size = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);
+
+
+    printf("Size of file: %lu bytes\n", size);
+
+
+    unsigned char buffer[BUFFER_SIZE];
+
+    // The total bytes read from the file, used as 
+    // an offset if file is not read in one go.
+    ssize_t total_bytes_read = 0;
+    ssize_t total_bytes_sent = 0;
+
+    ssize_t bytes_sent;
+    ssize_t bytes_read;
+
+    // Read contents of the file, and send as we go.
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
+
+        total_bytes_read += bytes_read;
+
+        // Send the contents to client
+        if ((bytes_sent = send(newfd, buffer, bytes_read, 0)) < 0) {
+            perror("Error sending buffer contents");
+            exit(EXIT_FAILURE);
+        }
+        //printf("%s", buffer);
+        total_bytes_sent += bytes_sent;
+    }
+
+
+    printf("Total bytes sent: %zu bytes\n", total_bytes_sent);
+
+    // Check that we have correctly sent the file.
+    if (total_bytes_read != size) {
+        fprintf(stderr, "Error reading file\n");
+        exit(EXIT_FAILURE);
+    } else if (total_bytes_sent != size) {
+        fprintf(stderr, "Error sending file\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
 
 
 /**
  * HTTP request handler function for a single worker thread.
+ * Responsible for fully constructing and sending a valid
+ * HTTP response to the client, after processing the request.
  */
 void *handle_http_request(void *arg) {
 
@@ -215,142 +342,7 @@ void *handle_http_request(void *arg) {
 }
 
 
-/**
- * Constructs the status line and headers to be sent in the HTTP
- * response, and then actually sends them to the client.
- */
-void send_response_head(int newfd, char *status_line, char *content_type) {    
 
-    // First construct the 'Content-Type' header, using the given content type
-    char *content_type_header = make_content_type_header(content_type);
-
-    // Calculate the size of the response head, + 1 for newline.
-    size_t size = strlen(status_line) + strlen(content_type_header) + 1;
-
-    // Allocate memory for response, +1 for nullbyte
-    char *response_head = malloc(sizeof(char)*(size+1));
-    if (response_head == NULL) {
-        perror("Error allocating memory to HTTP response");
-        exit(EXIT_FAILURE);
-    }
-
-    // Initialise memory so we can write over it safely
-    response_head[0] = '\0';
-
-
-    // Construct the HTTP response message status, header, and empty lines
-    strcat(response_head, status_line);
-    strcat(response_head, content_type_header);
-    strcat(response_head, "\n");
-    
-
-    // Now send the actual HTTP response status line and headers
-    ssize_t total_bytes_sent = 0, bytes_sent;
-    while ((bytes_sent = send(newfd, response_head+total_bytes_sent, 
-                              size-total_bytes_sent, 0)) > 0) {
-        total_bytes_sent += bytes_sent;
-    }
-
-    // Check to see if we've successfully sent the response head
-    if (total_bytes_sent != size) {
-        fprintf(stderr, "Error sending response head\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Free the memory allocated to content type header and response head
-    free(content_type_header);
-    free(response_head);
-}
-
-
-
-/**
- * Takes as input the file descriptors for an open connection
- * with a client, and one for a file, and sends the contents
- * of the file to the client.
- */
-void send_response_body(int newfd, int fd) {
-    // Don't send anything if file does not exist,
-    // empty message body.
-    if (fd < 0) {
-        return;
-    }
-
-    // The send buffer
-    unsigned char buffer[BUFFER_SIZE];
-
-    // Get the size of the file in bytes
-    size_t size = lseek(fd, 0, SEEK_END);
-    lseek(fd, 0, SEEK_SET);
-
-
-    printf("Size of file: %lu bytes\n", size);
-
-    // The total bytes read from the file, used as 
-    // an offset if file is not read in one go.
-    ssize_t total_bytes_read = 0;
-    ssize_t total_bytes_sent = 0;
-
-    ssize_t bytes_sent;
-    ssize_t bytes_read;
-
-    // Read contents of the file, and send as we go.
-    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
-
-        total_bytes_read += bytes_read;
-
-        // Send the contents to client
-        if ((bytes_sent = send(newfd, buffer, bytes_read, 0)) < 0) {
-            perror("Error sending buffer contents");
-            exit(EXIT_FAILURE);
-        }
-        //printf("%s", buffer);
-        total_bytes_sent += bytes_sent;
-    }
-
-
-    printf("Total bytes sent: %zu bytes\n", total_bytes_sent);
-
-    // Check that we have correctly sent the file.
-    if (total_bytes_read != size) {
-        fprintf(stderr, "Error reading file\n");
-        exit(EXIT_FAILURE);
-    } else if (total_bytes_sent != size) {
-        fprintf(stderr, "Error sending file\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-
-char *make_content_type_header(char *content_type) {
-    // Sanity checking
-    assert(content_type);
-
-    // The base size of the 'Content-Type' header,
-    // including two newline and one whitespace characters
-    const size_t base_size = 15;
-
-    // Calculate size of content type header (without nullbyte)
-    size_t content_type_header_size = base_size + strlen(content_type);
-
-    // Allocate and initialise memory for content type header
-    char *content_type_header;
-    content_type_header = malloc(sizeof(char)*(content_type_header_size+1));
-    if (content_type_header == NULL) {
-        perror("Error allocating memory for content type header");
-        exit(EXIT_FAILURE);
-    }
-    content_type_header[0] = '\0';
-
-
-    // Construct the header
-    strcat(content_type_header, "Content-Type: ");
-    strcat(content_type_header, content_type);
-    strcat(content_type_header, "\n");
-
-    return content_type_header;
-}
 
 
 /** MAIN FUNCTION **/
@@ -366,10 +358,8 @@ int main(int argc, char *argv[]) {
     char *port_num = argv[1];
     path_to_web_root = argv[2];
 
-
     int sockfd, newfd;
     struct addrinfo hints, *res;      // point to results
-
 
     memset(&hints, '\0', sizeof(hints)); // make sure struct is empty
     hints.ai_family   = AF_INET;        // use IPv4 address
@@ -397,7 +387,7 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        // Set option to allow reuse of a port if the server terminates.
+        // Set options to allow reuse of a port if the server terminates.
         int yes = 1;
         if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
             perror("Error setting socket options");
@@ -456,33 +446,19 @@ int main(int argc, char *argv[]) {
 
         // Check if connection was successfull
         if (newfd < 0) {
-            perror("ERROR on accept");
-            exit(EXIT_FAILURE);
+            perror("Error on accept");
+            continue;
         }
 
         printf("Successfully accepted a client connection request\n");
 
-
         pthread_t client_handler;
         pthread_create(&client_handler, NULL, handle_http_request, &newfd);
-
-        // UNCOMMENT THIS SHIT FOR ONLY ONE REQUEST
-        //pthread_join(client_handler, NULL);
-        //break;
-        
     }
 
-    
-
-    // If we don't care about a client's identity, can set client address and
-    // length parameters (last 2 parameters to accept) as NULL. Otherwise,
-    // before calling accept, we need to set addr parameter to a buffer large
-    // enough to hold the address, and set the integer pointed to by len to
-    // the size of the buffer in bytes.
-
-    
-    /* close socket */
+    // Close the socket
     close(sockfd);
 
+    // Done! 
     return 0;
 }
---
+++ server.h
@@ -0,0 +1,24 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+// Constants
+#define BUFFER_SIZE 8192  // buffer size
+#define BACKLOG 10        // total pending connections queue will hold
+
+// Global variable, to be shared by all threads
+char *path_to_web_root;
+
+// Function prototypes
+char *get_content_type(char *filename);
+void usage(char *prog_name);
+char *get_request_line(FILE *fdstream);
+char *get_filename(char *request_line);
+char *get_path_to_file(char *filename);
+char *get_content_type(char *filename);
+void *handle_http_request(void *arg);
+void send_response_body(int newfd, int fd);
+void send_response_head(int newfd, char *status_line, char *content_type);
+char *make_content_type_header(char *content_type);
+
+
+#endif
