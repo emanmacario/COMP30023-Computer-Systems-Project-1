c5dc9d8b5d2647039930b4f436b3400801ce52c0
emanmacario <emacario1997@hotmail.com>
2018-04-19 10:30:34 +1000

    Included the send_message() function prototype

--- server.h
+++ server.h
@@ -16,6 +16,7 @@ char *get_filename(char *request_line);
 char *get_path_to_file(char *filename);
 char *get_content_type(char *filename);
 void *handle_http_request(void *arg);
+ssize_t send_message(int newfd, char *message, ssize_t size);
 void send_response_body(int newfd, int fd);
 void send_response_head(int newfd, char *status_line, char *content_type);
 char *make_content_type_header(char *content_type);
